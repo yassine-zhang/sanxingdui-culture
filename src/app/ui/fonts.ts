527943a39e60cf9e06b719956b75ae523455554b
yassine-zhang <57878778@qq.com>
2025-02-26 09:40:41 +0800

    Initial Commit

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Noto_Sans_SC, Noto_Serif_SC } from "next/font/google";
+
+export const notoSansSC = Noto_Sans_SC({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
+
+export const notoSerifSC = Noto_Serif_SC({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
